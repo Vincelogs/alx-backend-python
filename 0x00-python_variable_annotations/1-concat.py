5726037501bd2951383699c292448269e432a79c
root <vincentharyor@gmail.com>
2023-10-05 15:25:29 -0700

    string concatenation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""type-annotated function for string concatenation"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat definition
+
+    Keyword arguments:
+    str1 -- string 1
+    str2 -- string 2
+    Return: concatenated string
+    """
+    return str1 + str2
+
