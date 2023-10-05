557cce81933de4f9bd1304085c9665a89ad1b2fd
root <vincentharyor@gmail.com>
2023-10-05 15:37:05 -0700

    float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""type-annotated function that returns the str rep of a float"""
+
+
+def to_str(n: float) -> str:
+    """to_str definition
+
+    Keyword arguments:
+    n -- float argument
+    Return: string representation of the float
+    """
+    return str(n)
