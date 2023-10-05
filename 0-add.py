c63526830dcc7e982f37c90d85acf85390551773
root <vincentharyor@gmail.com>
2023-10-05 15:10:11 -0700

    add

---
+++ 0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Basic annotations - add'''
+
+
+def add(a: float, b: float) -> float:
+    """adds two numbers using type annotation
+
+    Keyword arguments:
+    a -- float variable
+    b -- float variable
+    Return: sum of a and b as float
+    """
+    return a + b
